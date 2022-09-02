752bf6ca4f67813ceae77051de7f5be8079f5ca2
leowang <rfv7855659@gmail.com>
2022-09-03 04:39:43 +0800

    ✨ add esbuild-plugin/html-import-plugin

---
+++ esbuild-plugin/README.md
@@ -0,0 +1,6 @@
+# 學習如何使用 esbuild 開發插件
+
+## html-import-plugin
+對使用 CDN 引入的套件進行打包
+
+## html-plugin
---
+++ esbuild-plugin/build.js
@@ -0,0 +1,17 @@
+const {build} = require("esbuild")
+const httpImport = require("./html-import-plugin")
+async function runBuild(){
+  build({
+    absWorkingDir: process.cwd(),
+    entryPoints: ["./src/index.jsx"],
+    outdir: "dist",
+    bundle: true,
+    format: "esm",
+    splitting: true,
+    sourcemap: true,
+    metafile: true,
+    plugins: [httpImport()],
+  }).then(()=> console.log('build finished!'))
+}
+
+runBuild()
