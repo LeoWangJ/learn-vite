a6ed3f09dbf05c395639f557afbba87105f449b8
leowang <rfv7855659@gmail.com>
2022-09-08 18:08:19 +0800

    ✨ add transform , resolve

---
+++ mini-vite/src/node/plugins/resolve.ts
@@ -0,0 +1,60 @@
+import resolve from "resolve"
+import { Plugin } from "../plugin"
+import { ServerContext } from "../server"
+import path from 'path'
+import { pathExists } from "fs-extra"
+import { DEFAULT_EXTENSIONS } from '../constants'
+import { cleanUrl } from "../utils"
+
+
+export function resolvePlugin(): Plugin {
+    let serverContext: ServerContext
+    return {
+        name: 'm-vite:resolve',
+        configureServer(s) {
+            serverContext = s
+        },
+        async resolveId(id: string, importer?: string) {
+
+            if (path.isAbsolute(id)) {
+                if (await pathExists(id)) {
+                    return { id }
+                }
+
+                // 加上 root 路徑前綴、處理 /src/main.tsx 情況
+                id = path.join(serverContext.root, id)
+                if (await pathExists(id)) {
+                    return { id }
+                }
+            } else if (id.startsWith('.')) {
+                if (!importer) {
+                    throw new Error('`importer` should not be undefined')
+                }
+                const hasExtension = path.extname(id).length > 1
+                let resolveId: string
+                // 包含文件名後綴 ex ./App.tsx
+                if (hasExtension) {
+                    resolveId = resolve.sync(id, { basedir: path.dirname(importer) })
+                    if (await pathExists(resolveId)) {
+                        return { id: resolveId }
+                    }
+                } else {
+                    // 不包含文件名後綴 ex : ./App
+                    for (const extname of DEFAULT_EXTENSIONS) {
+                        try {
+                            const withExtension = `${id}${extname}`
+                            resolveId = resolve.sync(withExtension, { basedir: path.dirname(importer) })
+                            if (await pathExists(resolveId)) {
+                                return { id: resolveId }
+                            }
+                        } catch (e) {
+                            continue
+                        }
+                    }
+                }
+
+            }
+            return null
+        }
+    }
+}
---
+++ mini-vite/src/node/utils.ts
@@ -0,0 +1,18 @@
+import { HASH_RE, JS_TYPES_RE, QUERY_RE } from './constants'
+import path from 'path'
+
+export const isJSRequest = (id: string): boolean => {
+    id = cleanUrl(id)
+    if (JS_TYPES_RE.test(id)) {
+        return true
+    }
+    if (!path.extname(id) && !id.endsWith("/")) {
+        return true
+    }
+    return false
+}
+
+
+export const cleanUrl = (url: string): string => {
+    return url.replace(HASH_RE, "").replace(QUERY_RE, "")
+}
