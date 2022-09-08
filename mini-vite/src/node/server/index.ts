4e7c2ce027a7fe689e73abb13330a5d1ed0fcaf1
leowang <rfv7855659@gmail.com>
2022-09-08 14:32:20 +0800

    ✨add pluginContainer

--- mini-vite/src/node/server/index.ts
+++ mini-vite/src/node/server/index.ts
@@ -1,14 +1,39 @@
 import connect from 'connect'
-import {blue,green} from 'picocolors'
+import { blue, green } from 'picocolors'
 import { optimize } from '../optimizer'
+import { resolvePlugins } from '../plugins'
+import { createPluginContainer, PluginContainer } from '../pluginContainer'
+import { Plugin } from '../plugin'
+export interface ServerContext {
+  root: string
+  pluginContainer: PluginContainer
+  app: connect.Server
+  plugins: Plugin[]
+}
 
-export async function startDevServer(){
+export async function startDevServer() {
   const app = connect()
   const root = process.cwd()
   const startTime = Date.now()
-  app.listen(8484,async()=>{
+  const plugins = resolvePlugins()
+  const pluginContainer = createPluginContainer(plugins)
+
+  const serverContext: ServerContext = {
+    root: process.cwd(),
+    app,
+    pluginContainer,
+    plugins
+  }
+
+  for (const plugin of plugins) {
+    if (plugin.configureServer) {
+      await plugin.configureServer(serverContext)
+    }
+  }
+
+  app.listen(8484, async () => {
     await optimize(root)
-    console.log(green('No Bundle Server Running!'),`cost ${Date.now() - startTime}ms time`)
+    console.log(green('No Bundle Server Running!'), `cost ${Date.now() - startTime}ms time`)
     console.log(`> localhost: ${blue("http://localhost:8484")}`)
   })
 }
